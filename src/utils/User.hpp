dd8cc0c6b703d7ae9c546dc2b23a3e71cf2bfc26
daladim <daladim@users.noreply.github.com>
2020-04-26 23:43:23 +0200

    Added the User typedef

---
+++ src/utils/User.hpp
@@ -0,0 +1,13 @@
+#ifndef _USER_HPP_
+#define _USER_HPP_
+
+#include <iostream>
+
+namespace LogSupervisor{
+
+//! This represents a user
+typedef std::string User;
+
+} // namespace
+
+#endif //_USER_HPP_

0f895c19d0174135e57cf6db795dfca8ff4b0d8e
daladim <daladim@users.noreply.github.com>
2020-04-27 10:32:32 +0200

    Added a (more or less dummy) User class

---
+++ src/utils/User.hpp
@@ -0,0 +1,13 @@
+#ifndef _USER_INTERFACE_HPP_
+#define _USER_INTERFACE_HPP_
+
+#include <iostream>
+
+namespace LogSupervisor{
+
+//! This represents a single user
+typedef std::string User;
+
+} // namespace
+
+#endif //_USER_INTERFACE_HPP_

0d9b2e922775f79bfb5c9599e2090bb0b2285bc2
daladim <daladim@users.noreply.github.com>
2020-05-04 18:00:17 +0200

    Testing IPv6 addresses

--- tests/TestAddresses.cpp
+++ tests/TestAddresses.cpp
@@ -8,6 +8,7 @@ using namespace LogSupervisor;
 
 TEST_CASE( "IPv4 single addresses" ){
     CHECK_THROWS( Address("10.11.12.13/8") );
+    CHECK_THROWS( Address("This is not an address") );
 
     // Constructors
     const char c_str_addrOne[] = "192.168.0.1";
@@ -22,6 +23,7 @@ TEST_CASE( "IPv4 single addresses" ){
 static void check_contains_ipv4(const AddressRange& range){
     CHECK( range.contains(Address("10.20.30.40")) == true );
     CHECK( range.contains(Address("1.2.3.4")) == false );
+    CHECK( range.contains(Address("::1")) == false );
 }
 
 TEST_CASE( "IPv4 address ranges" ) {
@@ -39,17 +41,43 @@ TEST_CASE( "IPv4 address ranges" ) {
     // to_string
     CHECK( range.to_string()->compare(c_str_range) == 0 );
     CHECK( range2.to_string()->compare(str_range) == 0 );
-
 }
 
 
 
 TEST_CASE( "IPv6 single addresses" ){
+    CHECK_THROWS( Address("::1/8") );
+    CHECK_THROWS( Address("This is not an address") );
 
-
+    // Constructors
+    const char c_str_addrOne[] = "2001:2222:2222:b10c::1";
+    const string str_addrTwo("2001:2222:2222:b10c::99");
+    Address addr(c_str_addrOne);
+    Address addr2(str_addrTwo);
 }
 
 
+static void check_contains_ipv6(const AddressRange& range){
+    CHECK( range.contains(Address("2001:2222:2222:b10c::1")) == true );
+    CHECK( range.contains(Address("2001:2222:2222:b10c::99")) == true );
+    CHECK( range.contains(Address("2001:2222:2222:b10c:11:12:13:14")) == true );
+    CHECK( range.contains(Address("2001:2222:2222:b10c:11::ffff")) == true );
+    CHECK( range.contains(Address("1.2.3.4")) == false );
+    CHECK( range.contains(Address("2001:9999:9999:b10c:11::ffff")) == false );
+}
 TEST_CASE( "IPv6 address ranges" ) {
+    // Constructors
+    const char c_str_range[] = "2001:2222:2222:b10c::/64";
+    AddressRange range(c_str_range);
 
+    const string str_range("2001:2222:2222:b10c::/64");
+    AddressRange range2(str_range);
+
+    // Contains
+    check_contains_ipv6(range);
+    check_contains_ipv6(range2);
+
+    // to_string
+    CHECK( range.to_string()->compare(c_str_range) == 0 );
+    CHECK( range2.to_string()->compare(str_range) == 0 );
 }

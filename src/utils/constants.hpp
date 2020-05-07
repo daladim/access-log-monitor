19acd7d7f1aab08369faf5d8083782405f22db01
daladim <daladim@users.noreply.github.com>
2020-05-07 16:16:31 +0200

    Added a constants.hpp

---
+++ src/utils/constants.hpp
@@ -0,0 +1,11 @@
+#ifndef _CONSTANTS_HPP_
+#define _CONSTANTS_HPP_
+
+//! Some constants and utils for the whole source
+
+#ifndef GIT_VERSION_STRING
+#define GIT_VERSION_STRING "<unknown>"
+#endif
+
+
+#endif // _CONSTANTS_HPP_
